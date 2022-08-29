4aa4acca01c89715f9482e6156fb247468131131
simon waronja <simon.waronja@student.moringaschool.com>
2022-08-29 15:29:33 +0300

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,18 @@
-## code your solution here. 
+# code your solution here. 
+require 'pry'
+class Cat 
+    attr_accessor :name
+    attr_writer :meow
+    attr_reader :meow
+    
+
+end
+
+
+maru = Cat.new
+maru.name = "Maru"
+maru.meow = "meow!"
+
+# binding.pry
+
+puts maru.meow
